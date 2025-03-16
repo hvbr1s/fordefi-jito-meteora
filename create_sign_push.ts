cf3163973819b2604fe21088e517816d7503f067
Dan Jaheny <danjaheny@mac.home>
2025-03-16 10:03:54 +0100

    refactoring

--- create_sign_push.ts
+++ create_sign_push.ts
@@ -3,6 +3,7 @@ import * as axios from 'axios';
 import { signWithApiSigner } from './signing/signer';
 import { createAndSignTx } from './utils/process_tx'
 import { pushToJito } from './jito/push_to_jito'
+import { createJupiterSwapTx } from './jupiter/serialize_swap'
 import dotenv from 'dotenv'
 
 
@@ -10,66 +11,28 @@ import dotenv from 'dotenv'
 dotenv.config()
 const accessToken = process.env.FORDEFI_API_TOKEN;
 
-// Fetch serialized tx from json file
-const requestJson = JSON.parse(fs.readFileSync('./txs/serialized_tx.json', 'utf-8'));
-const requestBody = JSON.stringify(requestJson);
-
-// Define endpoint and create timestamp
-// const pathEndpoint = '/api/v1/transactions';
-const pathEndpoint = '/api/v1/transactions/create-and-wait';
-const timestamp = new Date().getTime();
-const payload = `${pathEndpoint}|${timestamp}|${requestBody}`;
-
-// Call 
-async function ping(
-  path: string,
-  accessToken: string,
-  payload: string
-): Promise<axios.AxiosResponse> {
-
-  // Send tx payload to API Signer for signature
-  const signature = await signWithApiSigner(payload);
-
-  try {
-
-    // Send signed payload to Fordefi for MPC signature
-    const respTx = await createAndSignTx(path, accessToken, signature, timestamp, requestBody);
-
-    // Throws for non-2xx status codes
-    if (respTx.status < 200 || respTx.status >= 300) {
-      throw new Error(`HTTP error status code: ${respTx.status}`);
-    }
-
-    return respTx;
-
-  } catch (error: any) {
-    // If the error is an Axios error, we can parse its details
-    if (error.response) {
-      const statusCode = error.response.status;
-      let errorMessage = `HTTP error occurred: status = ${statusCode}`;
-      try {
-        // If response data is JSON, attach it
-        const errorDetail = error.response.data;
-        errorMessage += `\nError details: ${JSON.stringify(errorDetail)}`;
-      } catch (jsonErr) {
-        // If the response is not JSON, attach raw text
-        errorMessage += `\nRaw response: ${error.response.data}`;
-      }
-      throw new Error(errorMessage);
-    }
-    // Network or other unknown error
-    throw new Error(`Network error occurred: ${String(error)}`);
-  }
-}
-
 async function main(): Promise<void> {
   if (!accessToken) {
     console.error('Error: FORDEFI_API_TOKEN environment variable is not set');
     return;
   }
+  // We create the tx (In this case the JUpiter swap but you can change it)
+  const jsonBody = await createJupiterSwapTx()
+
+  // Fetch serialized tx from json file
+  const requestBody = JSON.stringify(jsonBody);
+
+  // Define endpoint and create timestamp
+  const pathEndpoint = '/api/v1/transactions/create-and-wait';
+  const timestamp = new Date().getTime();
+  const payload = `${pathEndpoint}|${timestamp}|${requestBody}`;
 
   try {
-    const response = await ping(pathEndpoint, accessToken, payload);
+      // Send tx payload to API Signer for signature
+    const signature = await signWithApiSigner(payload);
+    
+    // Send signed payload to Fordefi for MPC signature
+    const response = await createAndSignTx(pathEndpoint, accessToken, signature, timestamp, requestBody);
     const data = response.data;
 
     // FOR DEBUGGING
--- signing/signer.ts
+++ signing/signer.ts
@@ -9,6 +9,7 @@ export async  function signWithApiSigner(payload: string): Promise<string> {
   const privateKey = crypto.createPrivateKey(privateKeyPem);
   const sign = crypto.createSign('SHA256').update(payload, 'utf8').end();
   const signature = sign.sign(privateKey, 'base64');
+  console.log("Payload signed -> ", signature)
 
   return signature
 }
